151d4894641a7ca4d32c8025c89f9d7edf417334
Siddeh <siddesha094@gmail.com>
2021-09-01 11:37:27 +0530

    branching

--- TestProject/src/MyFirs.java
+++ TestProject/src/MyFirs.java
@@ -1,6 +1,10 @@
 
 public class MyFirs {
 
+	public  void newTest() 
+	{
+		System.out.println("sys new ");
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Git demo");
@@ -10,6 +14,9 @@ System.out.println("Git demo");
 System.out.println("Git demo 2nd commit");
 System.out.println("3rd commit");
 System.out.println("3rd commit 2");
+System.out.println("Develop branch");
+System.out.println("Siddesh Dveloper");
+
 
 	}
 
